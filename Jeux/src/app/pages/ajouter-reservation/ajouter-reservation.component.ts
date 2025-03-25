4d3acebfa8ab64543bc2ff5ddd05171ff5c82478
Etienne Dumai <etienne.dumai@gmail.com>
2025-03-25 14:44:17 +0100

    feat(style.css angular.json package.json.lock): ajout angular material, liste-reservations pageReser
    Ajout de angular matérial pour gratter le plus de points possible, ajout des pages de liste des
    reservations, ajouter reservation, ajout component : header

---
+++ Jeux/src/app/pages/ajouter-reservation/ajouter-reservation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ajouter-reservation',
+  standalone: true,
+  imports: [],
+  templateUrl: './ajouter-reservation.component.html',
+  styleUrl: './ajouter-reservation.component.css'
+})
+export class AjouterReservationComponent {
+
+}
