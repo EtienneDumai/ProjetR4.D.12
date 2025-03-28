7751362a009b9e9289def31df5bad09bcbbd8350
Etienne Dumai <etienne.dumai@gmail.com>
2025-03-28 16:53:06 +0100

    feat(ajouter-reservation.ts.html): ajout liste déroulante dynamique
    Tout les jeux sont dans un liste déroulante material et change si le nombre de jeux change

--- Jeux/src/app/pages/ajouter-reservation/ajouter-reservation.component.ts
+++ Jeux/src/app/pages/ajouter-reservation/ajouter-reservation.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../composants/header/header.component';
 import { ReservationService } from '../../services/reservation/reservation.service';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
+import { JeuVideoService } from '../../services/jeu-video/jeu-video.service';
+import { JeuVideo } from '../../models/jeu-video.model';
 
 @Component({
   selector: 'app-ajouter-reservation',
@@ -13,8 +15,9 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './ajouter-reservation.component.html',
   styleUrl: './ajouter-reservation.component.css'
 })
-export class AjouterReservationComponent {
+export class AjouterReservationComponent implements OnInit {
   private readonly reservationService: ReservationService = inject(ReservationService);
+  private readonly jeuVideoService: JeuVideoService = inject(JeuVideoService);
   public formReservation: FormGroup = new FormGroup({
     nomClient: new FormControl('', [Validators.required]),
     emailClient: new FormControl('', [Validators.required]),
@@ -22,6 +25,20 @@ export class AjouterReservationComponent {
     idJeuReserve: new FormControl('', [Validators.required]),
     plateforme: new FormControl('', [Validators.required]),
     dateDeReservation: new FormControl('', [Validators.required]),
-    statutReservation: new FormControl('', [Validators.required])
+    statutReservation: new FormControl('', [Validators.required]),
+
   });
+  listeJeux: JeuVideo[] = [];
+  ngOnInit(): void {
+    this.jeuVideoService.getJeuxVideo().subscribe((jeux: JeuVideo[]) => {
+      this.listeJeux = jeux;
+    });
+  }
+  selectedNumPilote: number = 0;
+  onOptionChangeNumPilote(event: any) {
+    //Récupérer la valeur sélectionnée du numéro de pilote
+    this.selectedNumPilote = Number(event.target.value);
+    //Afficher la valeur sélectionnée du numéro de pilote dans la console pour verifier que tout se passe bien
+    console.log('voici le jeu' + this.selectedNumPilote);
+  }
 }
