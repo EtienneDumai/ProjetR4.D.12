61672e525d578c3151bab2bd03dc246c02e3be63
EtienneDumai <etienne.dumai@gmail.com>
2025-04-03 20:56:43 +0200

    feat(ajout-jeux): ajout du form complet
    A faire onSubmit()

--- Jeux/src/app/app.routes.ts
+++ Jeux/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AjouterReservationComponent } from './pages/reservation/ajouter-reserva
 import { PageReservationsComponent } from './pages/reservation/page-reservations/page-reservations.component';
 import { ModifierReservationComponent } from './pages/reservation/modifier-reservation/modifier-reservation.component';
 import { PageJeuxComponent } from './pages/jeux/page-jeux/page-jeux.component';
+import { AjouterJeuxComponent } from './pages/jeux/ajouter-jeux/ajouter-jeux.component';
 
 export const routes: Routes = [
   { path: '', component: PageReservationsComponent }, // Accueil par défaut
@@ -11,7 +12,8 @@ export const routes: Routes = [
   { path: 'ajouter-reservation', component: AjouterReservationComponent },
   { path: 'liste-reservation/edit/:id', component: ModifierReservationComponent },
   { path: 'page-jeux', component: PageJeuxComponent },
-  { path: 'page-jeux/edit/:id', component: ModifierReservationComponent }
+  { path: 'page-jeux/edit/:id', component: ModifierReservationComponent },
+  { path: 'ajouter-jeux', component: AjouterJeuxComponent }
 ];
 
 export const appRoutingProviders = [
--- Jeux/src/app/pages/jeux/ajouter-jeux/ajouter-jeux.component.ts
+++ Jeux/src/app/pages/jeux/ajouter-jeux/ajouter-jeux.component.ts
@@ -3,31 +3,52 @@ import { JeuVideo } from '../../../models/jeu-video.model';
 import { HttpService } from '../../../services/http.service';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-ajouter-jeux',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatFormField, MatSelectModule, MatInputModule, MatDatepickerModule, MatButtonModule],
+  providers: [
+      provideNativeDateAdapter(),
+      { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' }
+    ],
   templateUrl: './ajouter-jeux.component.html',
   styleUrl: './ajouter-jeux.component.css'
 })
 export class AjouterJeuxComponent {
+  listePlateforme: string[] = [];
   listeJeux: JeuVideo[] = [];
+  listeGenre: string[] = [];
+  thumbRegex!: RegExp;
   private readonly httpService: HttpService = inject(HttpService);
   private readonly router: Router = inject(Router);
   public formJeu: FormGroup = new FormGroup({
-    titre: new FormControl('', [Validators.required]),
+    titre: new FormControl('', [Validators.required, Validators.minLength(3)]),
     plateforme: new FormControl('', [Validators.required, Validators.email]),
-    genre: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]),
-    developpeur: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+    genre: new FormControl('', [Validators.required]),
+    developpeur: new FormControl('', [Validators.required]),
     dateDeSortie: new FormControl('', [Validators.required]),
     stock: new FormControl('', [Validators.required]),
-    imageUrl: new FormControl('', [Validators.required]),
+    imageUrl: new FormControl('', [Validators.required,  Validators.pattern(this.thumbRegex)]),
   });
   ngOnInit(): void {
     this.httpService.getJeuxVideo().subscribe((jeux: JeuVideo[]) => {
       this.listeJeux = jeux;
       console.log('Liste des jeux:', this.listeJeux);
     });
+    this.thumbRegex = new RegExp(
+      'https?:\\/\\/.*\\.(?:png|jpg|jpeg|gif|svg|webp)$'
+    );
+  this.listePlateforme = ['PC', 'PS4', 'PS5', 'XBOX ONE', 'XBOX SERIES X', 'SWITCH', 'NINTENDO DS'];
+  this.listeGenre = ['Action', 'Aventure', 'RPG', 'FPS', 'Simulation', 'Stratégie', 'Sport', 'Course'];
+  }
+  onSubmit(){
+
   }
 }
